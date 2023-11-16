749322e6f2d61d68fb08c9a9893c292c89001487
Alexandr Andreyev <mr.w1zard@mail.ru>
2023-11-16 21:44:25 +0600

    remove http request functions

--- rk7client.go
+++ rk7client.go
@@ -1,12 +1,7 @@
 package rk7client
 
 import (
-	"bytes"
 	"crypto/tls"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
 	"time"
 )
@@ -30,52 +25,4 @@ func NewClient(CashServerIP string, Port int, UserName, Password string) *Client
 			Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 		},
 	}
-}
-
-func (c *Client) newRequest(method string, body interface{}) (*http.Request, error) {
-	url := fmt.Sprintf("https://%s:%d/rk7api/v0/xmlinterface.xml", c.CashServerIP, c.CashServerPort)
-
-	var buf io.ReadWriter
-	if body != nil {
-		buf = new(bytes.Buffer)
-		err := json.NewEncoder(buf).Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
-	req, err := http.NewRequest(method, url, buf)
-	if err != nil {
-		return nil, err
-	}
-	if body != nil {
-		req.Header.Set("Content-Type", "application/xml")
-	}
-	req.Header.Set("Accept", "application/xml")
-	return req, nil
-}
-
-func (с *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
-	resp, err := с.HTTPClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-		body, err := io.ReadAll(resp.Body)
-
-		fmt.Println(string(body))
-		if err != nil {
-			return nil, err
-		}
-		return resp, err
-	}
-	if resp.StatusCode == 401 {
-		s := fmt.Sprintf("Unauthorized, status: %d", resp.StatusCode)
-		err = errors.New(s)
-		return nil, err
-	}
-	s := fmt.Sprintf("Unknown error. Status: %d", resp.StatusCode)
-	err = errors.New(s)
-	return nil, err
 }
