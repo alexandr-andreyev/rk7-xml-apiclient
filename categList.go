393f591cd0cf0fddb093684faf17530d3b7e0d6b
mr.w1zard@mail.ru <mr.w1zard@mail.ru>
2024-07-23 14:34:15 +0500

    fix to const

--- categList.go
+++ categList.go
@@ -4,10 +4,10 @@ func (c Client) GetCateglist() (*RK7QueryResult, error) {
 	cmd := RK7Query{
 		RK7Command: []RK7Command{
 			{
-				CMD:            "GetRefData",
-				RefName:        "CATEGLIST",
-				OnlyActrive:    "true",
-				WithChildItems: "0",
+				CMD:            RK7CMD_GETREFDATA,
+				RefName:        RK7REF_CATEGLIST,
+				OnlyActrive:    ONLY_ACTIVE_TRUE,
+				WithChildItems: WITHCHILDITEMS_NO_CHILDREN,
 				PropMask:       "items.(Ident,GUIDString,Code,Name,MainParentIdent,Status,Parent)",
 			},
 		},
@@ -18,6 +18,8 @@ func (c Client) GetCateglist() (*RK7QueryResult, error) {
 	}
 	result := RK7QueryResult{}
 	_, err = c.do(req, &result)
-	//defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
 	return &result, nil
 }
